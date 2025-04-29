3abcb59c294aef97ea994688e9287cb732787474
unknown <lynnethdelacerna6@gmail.com>
2025-04-29 22:30:53 +0800

    another

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,6 @@
 
 import { Container, Row, Col } from "react-bootstrap";
 import { ArrowRightCircle } from 'react-bootstrap-icons';
-import 'animate.css';
 import TrackVisibility from 'react-on-screen';
 
 export const Banner = () => {
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -36,7 +36,6 @@ import laagain16 from "../assets/img/laagain16.png";
 import laagain17 from "../assets/img/laagain17.png";
 import laagain18 from "../assets/img/laagain18.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
-import 'animate.css';
 import TrackVisibility from 'react-on-screen';
 import React, { useState } from 'react';
 
